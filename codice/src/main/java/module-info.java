3ba644f2951d3ac71dccfc0560b046448cab542c
Denny Caruso <41870472+dennewbie@users.noreply.github.com>
2022-01-16 18:47:24 +0100

    Added intellij project basic stuff

---
+++ codice/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.prog3.ipt {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.prog3.ipt to javafx.fxml;
+    exports com.prog3.ipt;
+}
