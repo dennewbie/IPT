4286cc95100cd0255c5bb885737744b6578ce0df
Denny Caruso <41870472+dennewbie@users.noreply.github.com>
2022-01-28 16:31:19 +0100

    added LoginRegisterView and basic navigation between HomeView and LoginRegisterView
    ... uncomplete still working

--- codice/src/main/java/com/prog3/ipt/Controller/HomeViewController.java
+++ codice/src/main/java/com/prog3/ipt/Controller/HomeViewController.java
@@ -1,10 +1,18 @@
 package com.prog3.ipt.Controller;
 
+import com.prog3.ipt.IPT_Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeViewController {
     @FXML
@@ -20,6 +28,10 @@ public class HomeViewController {
     @FXML
     private Button avvisiUtenzaButton;
 
+    private FXMLLoader fxmlLoader;
+    private Scene scene;
+    private Stage stage;
+
     @FXML
     void onInfoButtonClick(ActionEvent event) {
 
@@ -27,7 +39,14 @@ public class HomeViewController {
 
     @FXML
     void onLoginButtonClick(ActionEvent event) {
-
+        try {
+            fxmlLoader = new FXMLLoader(IPT_Application.class.getResource("LoginRegisterView.fxml"));
+            scene = new Scene(fxmlLoader.load(), 1080, 720);
+            stage = (Stage) loginButton.getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
