0ebce5afd7681046d54a0f96a626337ae1f7ab50
Maax <massimiliano.giordanoorsini001@studenti.uniparthenope.it>
2022-02-01 13:27:01 +0100

    update - added dependencies

--- codice/src/main/java/com/prog3/ipt/Controller/NoticesViewController.java
+++ codice/src/main/java/com/prog3/ipt/Controller/NoticesViewController.java
@@ -1,9 +1,7 @@
 package com.prog3.ipt.Controller;
 
-import com.prog3.ipt.Model.FacadeClasses.DatabaseConnectionSingleton;
 import com.prog3.ipt.Model.FacadeClasses.FacadeSingleton;
 import com.prog3.ipt.Model.Notice;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,13 +11,8 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
-import java.sql.*;
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class NoticesViewController extends ViewController {
     // Navigation Bar
